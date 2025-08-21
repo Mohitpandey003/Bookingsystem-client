c3634c1c00d70145a27f2bf0229d6037d408c9b7
Mohitpandey003 <mohitraj2022003@gmail.com>
2025-08-21 14:38:03 +0530

    Fixed login route and other updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -9,54 +9,52 @@ function Login() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     try {
-      const res = await API.post("/Login",{ email, password });
+      const res = await API.post("/login", { email, password });
       if (res.data.message === "Login successful") {
-        localStorage.setItem("user",JSON.stringify(res.data));
-        alert("login successful");
+        localStorage.setItem("user", JSON.stringify(res.data));
+        alert("Login successful");
         navigate("/");
       } else {
-        alert(res.data || "Login failed");
+        alert(res.data.message || "Login failed");
       }
     } catch (err) {
-      console.error(err);
-      alert("something went worng");
+      console.error(err.response || err);
+      alert(err.response?.data?.message || "Something went wrong");
     }
   };
   return (
-            <div className='container mt-5'style ={{maxWidth: "500px"}}>
-            <h2 className='text-center mb-4'> Login</h2>    
-            <form onSubmit={handleSubmit}>
-                <div className='mb-3'>
-                    <label> Email address</label>
-                    <input 
-                    type="email"
-                    className="form-control"
-                    placeholder="Enter email"
-                    value = {email}
-                    onChange= {(e) => setEmail(e.target.value)}
-                    required
-                    />
-                </div>
+    <div className="container mt-5" style={{ maxWidth: "500px" }}>
+      <h2 className="text-center mb-4"> Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div className="mb-3">
+          <label> Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
 
-                <div className='mb-3'>
-                    <label>Password</label>
-                    <input 
-                    type="password"
-                    className="form-control" 
-                    placeholder='Enter password'
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
-                    />
-                </div>
-                <button type="submit" className="btn btn-primary w-100">
-                    Login
-                </button>
-            </form>
-
-            </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary w-100">
+          Login
+        </button>
+      </form>
+    </div>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API = axios.create({
-    baseURL: 'http://localhost:4000/api',
+    baseURL: 'https://bookingsystem-server-xydx.onrender.com/api',
     withCredentials:true,
 
 
